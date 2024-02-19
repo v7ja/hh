53abcfe636f13c89693911ad632d18c1da6e3db2
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-19 18:48:57 +0300

    Update ttol.py

--- ttol.py
+++ ttol.py
@@ -1,11 +1,10 @@
 import requests
 import random
 
-print(' @P_W_7 - @P_W_77')
+
+iD = input(' Enter ID Bot : ')
 print('\n')
-ID = input(' Enter ID Bot: ')
-print('\n')
-tok = input(' Enter Token Bot: ')
+token = input(' Enter Token Bot : ')
 print('\n')
 user = 'qwertyuiopasdfghjklzxcvbnm1234567890'
 #user = '1234567890'
@@ -17,7 +16,48 @@ def zz():
 	 	us = str("".join(random.choice(user)for x in range(1)))
 	 	um = str("".join(random.choice(user)for x in range(1)))
 	 	ur = str("".join(random.choice(user)for x in range(1)))
-	 	usery= us+'_'+um+'.'+ur
+	 	a1 = us+'.'+um+'.'+ur
+			a2 = us+'_'+um+'_'+ur
+			a3 = us+'_'+us+'.'+um
+			a4 = us+'_'+um+'_'us
+			a5 = us+'.'+um+'.'us
+			a6 = ur+'_'+um+'.'+us
+			a7 = ur+ur+um+um+'_'
+			a8 = ur+um+um+um+'_'
+			a9 = ur+ur+um+um+'.'
+			a10 = ur+um+um+um+'.'
+			a11 = ur+ur+ur+um+'_'
+			a12 = ur+ur+ur+um+'.'
+			a13 = '_'+ur+ur+um+um
+			a14 = '.'+ur+ur+um+ur
+			a15 = '_'+ur+um+um+um
+			a16 = '.'+um+um+um+ur
+			a17 = ur+'_'+ur+'_'+ur
+			a18 = ur+'.'+ur+'.'+ur
+			a19 = ur+'_'+us+'_'+ur
+			a20 = ur+'.'+us+'.'+ur
+			a21 = ur+'_'+ur+'_'+us
+			a22 = ur+'.'+ur+'.'+us
+			a23 = us+'_'+ur+'_'+ur
+			a24 = us+'.'+ur+'.'+ur
+			a25 = ur+'_'+um+'_'+ur
+			a26 = ur+ur+ur+'.'+us
+			a27 = ur+ur+ur+'_'+us
+			a28 = um+ur+ur+'.'+ur
+			a29 = um+ur+ur+'_'+ur
+			a30 = um+ur+ur+ur+ur
+			a31 = um+ur+ur+ur+um
+			a32 = us+ur+ur+ur+us
+			a33 = um+um+ur+ur+ur
+			a34 = us+us+ur+ur+ur
+			a35 = ur+us+ur+us+ur
+			a36 = '_'+ur+ur+ur+'_'
+			a37 = '.'+ur+ur+ur+'.'
+			a38 = '.'+um+um+um+'.'
+			a39 = ur+us+'_'+um
+			a40 = ur+ur+us+um
+			abd = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40)
+			usery = random.choice(abd)
 	 	url = 'https://i.instagram.com/api/v1/accounts/create/'
 	 	he = {
 	'Content-Length': '437',
@@ -45,20 +85,18 @@ def zz():
 	 	#exit()
 	 	if "username" in rr:
 	 		zok+=1
-	 		print(f' {zok} : Bad User : {usery}')
+	 		print(f' {zok} : BaD uSeR : {usery}')
 	 	elif 'email_is_taken' in rr:
 	 		
 	 		zok+=1
 	 		print(rr)
-	 		print(f' {zok} : Good User : {usery}')
-	 		tlg=f'''
-✅User Insta
-----------------------------
-user : {usery}
----------------------------
-From : @
-	'''
-	 		requests.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text="+str(tlg))
+	 		print(f' {zok} : DoNe uSeR : {usery}')
+	   abood = """
+𝖽𝗈𝗇𝖾 𝗎𝗌𝖾𝗋 𝖼𝗅𝗂𝗆𝖾𝖽 </>
+⦗ 𝗎𝗌𝖾𝗋 ⦘ | ⦗ @{usery} ⦘
+⦗ 𝗉𝗋𝗈𝗀𝗋𝖺𝗆𝗆𝖾𝗋 ⦘ | ⦗ @kckkkkc ⦘
+"""
+	 		requests.post(f"https://api.telegram.org/bot{token}/sendvideo?chat_id={iD}&video=https://t.me/m_aBd/14&caption="+str(abood))
 	 	else:
 	 		zok+=1
 	 		print(f' {zok} : Bad User : {usery}')
