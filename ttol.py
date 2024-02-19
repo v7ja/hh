5a7caa5f2ec4792d004932e414ebf8c94651e9d0
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-19 18:55:50 +0300

    Update ttol.py

--- ttol.py
+++ ttol.py
@@ -17,7 +17,6 @@ def zz():
 	 	 um = str("".join(random.choice(user)for x in range(1)))
 	 	 ur = str("".join(random.choice(user)for x in range(1)))
 	 	 a1 = us+'.'+um+'.'+ur
-	    a2 = us+'_'+um+'_'+ur
 			a3 = us+'_'+us+'.'+um
 			a4 = us+'_'+um+'_'us
 			a5 = us+'.'+um+'.'us
@@ -56,7 +55,7 @@ def zz():
 			a38 = '.'+um+um+um+'.'
 			a39 = ur+us+'_'+um
 			a40 = ur+ur+us+um
-			abd = (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40)
+			abd = (a1, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40)
 			usery = random.choice(abd)
 	 	url = 'https://i.instagram.com/api/v1/accounts/create/'
 	 	he = {
