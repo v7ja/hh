a291020d5092c8f9ccc66fe009ad63151a94ec9c
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-19 10:48:00 +0300

    Update ttol.py

--- ttol.py
+++ ttol.py
@@ -1,34 +1,131 @@
+import os
+import time
+try :
+    import requests
+    import user_agent
+    import datetime
+    import webbrowser
+    import pyfiglet
+    import threading
+    import signal
+except ImportError as error :
+    os.system('pip install requests')
+    os.system('pip install user_agent')
+    os.system('pip install datetime')
+    os.system('pip install webbrowser')
+    os.system('pip install pyfiglet')
+    os.system('pip install threading')
+    os.system('pip install signal')
+    os.system('cls'if os.name=='nt' else'clear')
+    time.sleep(1)
+    print('Done.')
+    exit()
+import instaloader
 import requests
+from time import sleep
+import os
+import sys
+from user_agent import generate_user_agent
+import datetime
 import random
-import threading
-import pyfiglet
+import requests
+from colorama import Fore,Back
+import time
+import sys as n
+import time as mm
+import secrets
+from user_agent import generate_user_agent
+os.system('clear')
+B="\033[1;30m" # Black
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+Bl="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # White
+print(C+   """  
+
+""")
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+Y = '\033[1;34m' #ازرق فاتح
+M = '\x1b[1;37m'#ابیض
+U = '\x1b[1;37m'#ابیض
+X = '\033[1;33m' #اصفر
+Y = '\033[1;34m' #ازرق فاتح
+M = '\x1b[1;37m'#ابیض
+S = '\033[1;33m'
+R = '\033[1;31m' #احمر
+F = '\033[2;32m' #اخضر
+C = "\033[1;97m" #ابيض
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح.
+C1 = '\033[2;35m'
+
+												
+
+print('')
+cookies=sid = input('SEASOON ID :')
+os.system("clear")
+H="\x1b[38;5;208m" #
 
 
-litters = 'qwertyuiopasdfghjklzxcvbnm1234567890'
-id = input("[+] iD : ")
-token = input("[+] ToKeN : ")
-abd = "Take > @{user} | bY : @kckkkkc"
-sessionid = input("EnTeR Session : ")
-def Lev ():
 
-	hea = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'user-agent': 'user-agent: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html',
-        'Cookie': 'sessionid='+sessionid,
-    }
-	while True:
-	   user = str("".join(random.choice(litters)for x in range(4)))
-	   tiko = f'https://www.tiktok.com/api/user/detail/?aid=1988&app_language=ar&app_name=tiktok_web&battery_info=0.32&browser_language=ar&browser_name=Mozilla&browser_online=true&browser_platform=MacIntel&browser_version=5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F109.0.0.0%20Safari%2F537.36&channel=tiktok_web&cookie_enabled=true&device_id=7193110014067459586&device_platform=web_pc&focus_state=true&from_page=user&history_len=10&is_fullscreen=false&is_page_visible=true&language=ar&os=mac&priority_region=&referer=&region=SA&screen_height=900&screen_width=1440tz_name=Asia%2FRiyadh&uniqueId={user}&verifyFp=verify_ldvov399_du9goymx_OHxC_4RTw_AEjU_Dth4CFGFw3lR&webcast_language=ar&msToken=f7RQRFGwBsu3WXbrhdLVX9gDRSynM_O_C7U9SX6WNqZqmb0QEsNO6H3dJ10pMAxt24bmyb2eMNPzUpr8w8-6Wx-xAawe1R6vbD6HZdDoWTPL4VOHo6ebwjHadXlUoyhG9ovbpBnhHipd_EWG&X-Bogus=DFSzswVY9D0ANeIIShUJbR/F6qHH&_signature=_02B4Z6wo00001xH2Y0gAAIDCaTiITAKYgosR9mfAAKeo28'
-	   reqsnd = requests.get(tiko, headers=hea).text
-	   if '"statusCode":10221,' in reqsnd:
-	           print(f'{user} Done ⭐️')
-	           requests.post(f"https://api.telegram.org/bot{token}/sendvideo?chat_id={iD}&video=https://t.me/m_aBd/14&caption="+str(abd))
-	   else:
-	   	print(f'{user} BaD ')
-Threads=[] 
-for t in range(10):
-    x = threading.Thread(target=Lev)
-    x.start()
-    Threads.append(x)
-for Th in Threads:
-    Th.join()
+def HANI():
+ head={'Cookie':'mid=YF55GAALAAF55lDR3NkHNG4S-vjw; ig_did=F3A1F3B5-01DB-457B-A6FA-6F83AD1717DE; ig_nrcb=1; shbid=13126; shbts=1616804137.1316793; rur=PRN; ig_direct_region_hint=ATN; csrftoken=ot7HDQ6ZX2EPbVQe1P9Nqvm1WmMkzKn2; ds_user_id=46165248972; sessionid='+sid}
+ while True:
+#  mal=['male','femal']
+#  gen=random.choice(mal)
+  user='angel.gator'
+  num='123'
+  rng=int("".join(random.choice(num)for i in range(1)))
+  name=str("".join(random.choice(user)for i in range(rng)))
+  hani=requests.get(f'https://www.instagram.com/web/search/topsearch/?context=blended&query={name}',headers=head).json()
+  hhan = 0
+  
+  try:
+    while True:
+    	global a,z,t,x,ll
+    	hhan+=1
+    	email = user=str(hani['users'][hhan]['user']['username'])
+    	a+=1
+    	print(f'\033[1;33m[{a}]«--» : [{email}] ')
+    	user = email+'@hotmail.com'
+    	with open('user123.txt', 'a') as (HA):
+	   		HA.write(f'{user}\n')
+  except IndexError:
+  	print(Z+'STOP Event Error ')     
+          
+        		
+	
+	
+
+                                      
+                                   
+print(f"""
+{B}Welcome to the hunting tool insta   
+{Z}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓           
+{X}┃   {M}❖ @ K_Q_A {Z}         ➠         {M}@ H_6_N{X}  
+{F}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛               """)
+print(' ➘  ')
+print('\033[1;33m ســحب لـــســتــه عشوائي [1]\nحـــــــــــذف لـــــــــســــتــــه [0]')
+try:
+	print('')
+	HA = int(input(' \033[1;36m  : اختار رقم للاستمرار [❖]  '))
+	system("clear")
+except ValueError as error:
+	system('clear')
+	print('Error - خـطـا')
+	exit()
+if HA ==1:
+	HANI()		
+elif HA ==0:
+    O()	 
+else:
+	system('clear')
+	print('Error')
+	exit()
