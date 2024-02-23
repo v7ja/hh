c42066b8905aed1110d0d8b9b5075b094c166174
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-23 12:29:37 +0300

    Update ttol.py

--- ttol.py
+++ ttol.py
@@ -1,66 +1,46 @@
-import requests, random, datetime
-from colorama import Fore
-R = Fore.RED
-P = Fore.GREEN
-s = Fore.LIGHTRED_EX
-y = Fore.YELLOW
-fz = datetime.datetime.now()
+import requests
+
 logo =f""" 
-                     <cHecker Tik v3>    
-         ╔════════════════════════════════════════╗
-         ║      <\> Programmer : @kckkkkc         ║
-         ║      <\> iG : @kkkcckc                 ║
-         ╚════════════════════════════════════════╝
+          <Welcome To Session Tool>             ╔════════════════════════════════════════╗
+║      <\> Programmer : @kckkkkc         ║
+║      <\> iG : @kkkcckc                 ║         ╚════════════════════════════════════════╝
 """ ; print(logo)
-sa="4"
-if sa =="4":
-    uesr = ''   
-    chars2 = 'qwertyuiopasdfghjklzxcvbnm1234567890._'
-    amount = 5000
-    amount = int(amount)
 
-    length2 = 4
-    length2 = int(length2)
+user = input("EnTeR uSer : ")
+pess = input("EnTeR Pass : ")
 
-    for password in range(amount):
-        password = ''
+url='https://www.instagram.com/accounts/login/ajax/'
+h={
+	'accept': '*/*',
+	'accept-encoding': 'gzip, deflate, br',
+	'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
+	'content-length': '291',
+	'content-type': 'application/x-www-form-urlencoded',
+	'cookie': 'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=COmXgzKurrq8awSnRS1xf3u9rL6QsGZI',
+	'origin': 'https://www.instagram.com',
+	'referer': 'https://www.instagram.com/',
+	'sec-fetch-dest': 'empty',
+	'sec-fetch-mode': 'cors',
+	'sec-fetch-site': 'same-origin',
+	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+	'x-csrftoken': 'COmXgzKurrq8awSnRS1xf3u9rL6QsGZI',
+	'x-ig-app-id': '936619743392459',
+	'x-ig-www-claim': '0',
+	'x-instagram-ajax': '1cb44f68ffec',
+	'x-requested-with': 'XMLHttpRequest'}
+data={
+	'username': user,
+	'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:1613414957:{pess}', 
+	'queryParams': '{}',
+	'optIntoOneTap': 'false'}
+    
+req=requests.post(url,headers=h,data=data)
+	
+if '"authenticated":true' in req.text:
+	print('Done login')
+	sessd=req.cookies['sessionid']
+	print(sessd)
 
-        for item in range(length2):
-            password = ''
-        for item in range(length2):
-            password += random.choice(chars2)
-
-        
-        with open('user.txt', 'a') as x:
-            x.write(uesr + password + '\n')
-j = requests.get('https://pastebin.com/raw/jsXm5HCA').text
-x2 = requests.get('https://pastebin.com/raw/08NqbfeS').text
-use="user.txt"
-ID = input(R+"Enter Your id Account -> :")
-token = input(R+'EnTer Your Token ->:')
-headers={
-"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-"Connection": "close",
-"Host": "www.tiktok.com",
-"Accept-Encoding": "gzip, deflate",
-"Cache-Control": "max-age=0"}
-file = open(use, "r")
-while True:
-	Check = file.readline().split('\n')[0]
-	if Check == '':
-		break
-	tiklog = f'https://www.tiktok.com/@{Check}'
-	rq = requests.get(tiklog, headers=headers)
-	if rq.status_code == 404:
-		print(P+'[+] Available >> {}'.format(Check))
-		tele = (
-			f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={j}\n⦗ 𝘂𝘀𝗲𝗿 ⦘ | ⦗ {Check} ⦘\n{x2}')
-		re = requests.post(tele)
-		with open('Tik-HuNt.txt', 'a') as x:
-			x.write(Check + '\n')
-	elif rq.status_code == 200:
-		print(s+'[-] NoT Available >> {}'.format(Check))
-
-
-
+else:
+	print("error login check password or user ?")
+	
