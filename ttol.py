6f08e68f722e2e3d4e4d7a2b8d570b7cca3249f2
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-23 19:59:25 +0300

    Update ttol.py

--- ttol.py
+++ ttol.py
@@ -128,8 +128,7 @@ def usernameG():
   c = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
   a = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
   n = ''.join(choice('1234567890') for i in range(1))
-  return k+k+k+n+c
-k+k+k+a+n		
+  return k+k+k+n+c k+k+k+a+n
 # start checking
 def start(client,username):
   try:ok = fragment(username)
