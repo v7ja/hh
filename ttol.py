ad65b4eafeeceab41fd90f221e9a556d341c517b
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-23 23:12:27 +0300

    Update ttol.py

--- ttol.py
+++ ttol.py
@@ -125,10 +125,8 @@ def checker(username,client):
 # for generate username
 def usernameG():
   k = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
-  c = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
-  a = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
-  n = ''.join(choice('1234567890') for i in range(1))
- return k+a+k+k+a+k+k
+	n = ''.join(choice('1234567890') for i in range(2))
+	return k+k+k+n+k+k+n
 # start checking
 def start(client,username):
   try:ok = fragment(username)
